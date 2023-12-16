d31195b85d069f6222e9567d6f4aa00515f57ba0
shahinpn <153208688+shahinpn@users.noreply.github.com>
2023-12-16 19:59:44 +0330

    v1.2

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let master = document.querySelector(".master");
 let cd = document.querySelector(".cd");
+let loader = document.querySelector(".loader");
 
 let inp = document.querySelector("input");
 let gnr = document.querySelector("h3");
 
 
 function x(){
-    master.style.display = "none"
-    cd.style.display = "block"
-    gnr.innerText = `قد شما ${inp.value} سانتی متر است`
+    master.style.display = "none";
+    loader.style.display = "block";
+    setTimeout(function(){
+        loader.style.display = "none";
+        cd.style.display = "block";
+        gnr.innerText = `قد شما ${inp.value} سانتی متر است`;
+    }, 2000)
 }
